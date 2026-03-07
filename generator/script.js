2ff83e358a367f0de3e69bf577060226e2d7d23f
developer-sarvaroy <shermahamedovasarvaroy@gmail.com>
2026-03-07 03:56:46 +0300

    Random Color Generator project added

---
+++ generator/script.js
@@ -0,0 +1,17 @@
+function changeColor(){
+
+let letters = "0123456789ABCDEF";
+
+let color = "#";
+
+for(let i = 0; i < 6; i++){
+
+color += letters[Math.floor(Math.random() * 16)];
+
+}
+
+document.body.style.background = color;
+
+document.getElementById("colorCode").innerText = color;
+
+}
